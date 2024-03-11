a7286275e375c7674c9274e293cd32e60001fcd1
Crowdbotics <team@crowdbotics.com>
2024-03-11 15:42:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ubereatsnew-46926",
+  url: "https://ubereatsnew-46926.botics.co",
+  api: "https://ubereatsnew-46926.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
